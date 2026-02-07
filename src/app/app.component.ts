539a1f7f28314f472c235a790394eead2a626d10
thiago_osim <thiago_osim@hotmail.com>
2026-02-07 15:40:37 -0300

    Criado a estrutura do balance-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'layout-cards-base-angular';
+  balanceItems = [
+    { label: 'Conta Corrente', amount: 'R$ 200,00' },
+    { label: 'Conta Corrente Saldo Vinculados', amount: 'R$ 300,00' },
+    { label: 'Conta Poupança Saldo Vinculados', amount: 'R$ 400,00' },
+    { label: 'Investimento com Baixa Automática', amount: 'R$ 500,00' },
+    { label: 'Investimento sem Baixa Automática', amount: 'R$ 500,00' },
+  ];
+
+  balanceSummary = [
+    { label: 'SALDO TOTAL', amount: 'R$ 1000,00' },
+    { label: 'LIVRE PARA MOVIMENTAÇÃO', amount: 'R$ 1000,00' },
+  ];
+
 }
